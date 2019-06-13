d47b7dcdd04c7b7d1a6c55979e88ab5cbc4789fc
Aaron Myers <n10221743@qut.edu.au>
2019-06-13 17:47:32 +1000

    updating status codes and end points

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-// var db = require('./database/db');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,15 +19,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// importing middleware
 app.use(logger('dev'));
 app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
-// app.use(db);
 
+// importing middleware
 const option = require('./knexfile.js');
 const knex = require('knex')(option);
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,4 @@
 var express = require('express');
-// var mysql = require('mysql');
-
 var router = express.Router();
 
 /* GET home page. */
@@ -8,21 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api', function(req, res, next) {
-	res.render('index', {title: "Choose a Database to access"});
-});
-
+// GET requests to API
+// Returns a list of all the offence rows
 router.get('/offences', function(req,res,next) {
 	req.db.from('offence_columns').distinct('*')
 		.then((rows) => {
 			res.json({"Error": false, "Message" : "Success", "Offences" : rows});
 		})
 		.catch((err) => {
-			res.json({"Error": true, "Message": "Error executing MySQL query"});
+			res.status(400).json({"Error": true, "Message": "Error executing MySQL query"})
 		})
 });
 
+// Implements the search function without JWT authorization
 router.get('/search?', function(req, res, next) {
+	if( !req.query.offences ){
+		res.status(400).json({message: `oops! it looks like you're missing the offence query parm`})
+		throw new Error('OFFENCES');
+	}
 	let p = req.params.Param;
 	let query = req.db.select('area').sum({total: req.query.offence}).from('offences').groupBy('area')
 	if(req.query.area && req.query.area != ""){
@@ -48,11 +49,12 @@ router.get('/search?', function(req, res, next) {
 		query.then((rows) => {
 			res.json({"Error": false, "Message" : "Success", "result" : rows});
 		})
-		.catch((err) => {
-			res.json({"Error": true, "Message": "Error executing MySQL query", "Param": req.params.Param});
+	.catch((err) => {
+			res.json({"Error": true, "Message": "Error executing MySQL query"});
 		})
 });
 
+// checks for requests asking for specific data
 router.get('/:Param', function(req,res,next) {
 	console.log(req.query);
 	req.db.from('offences').distinct(req.params.Param)
@@ -62,6 +64,17 @@ router.get('/:Param', function(req,res,next) {
 		.catch((err) => {
 			res.json({"Error": true, "Message": "Error executing MySQL query"});
 		})
+});
+
+
+// Post Requests
+// checks for a register request
+router.post('/register', function(req,res,next) {
+	res.status(400).json({message: `oops! It looks like that user already exists :(`})
+});
+// checks for a login request
+router.post('/login', function(req,res,next) {
+	res.status(401).json({message: `invalid login - bad password`})
 });
 
 module.exports = router;
